1c86c53fb8e0b04e4db5e85fc6684c442c88fcd9
Egor Kolesnikov <freethinkel@gmail.com>
2024-12-09 20:57:50 +0500

    cleanup

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,7 +1,11 @@
+use std::time::Instant;
+
 mod first;
 mod second;
 
 fn main() {
+    let before = Instant::now();
     println!("PART_ONE: {}", first::run());
     println!("PART_TWO: {}", second::run());
+    println!("Elapsed time: {:.2?}", before.elapsed());
 }
